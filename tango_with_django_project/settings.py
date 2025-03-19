75300a57faeeba6d565dc6d1c306532cd0809c51
XiangbinYOU <2287832046@qq.com>
2025-03-19 16:57:23 +0800

    Remedial content (Course, searches, notes)

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -137,3 +137,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 LOGIN_URL = 'rango:login'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
