e02ca98a37b0d81404bb9bb5f33613e535d83494
“Zicheng <2915413y@student.gla.ac.uk>
2025-03-27 03:02:41 +0000

    views&urls&templates_clean_no_use_lines_AND_improve_user_experience

--- rango/urls.py
+++ rango/urls.py
@@ -7,18 +7,16 @@ app_name = 'rango'
 
 urlpatterns = [
     path('', views.tNindex, name='tNindex'),
-    
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-    path('restricted/', views.restricted, name='restricted'),
-    
-    path('course/', views.tNcourse, name='tNcourse'),
-    path('note/<int:NoteID>/', views.tNnote, name='tNnote'),
-    path('notes/<str:CourseID>/', views.tNnotes, name='tNnotes'),
-    path('search/', views.tNsearch, name='tNsearch'),
-    path('upload/', views.tNupload, name='tNupload'),  # For new uploads
-    path('upload/<int:NoteID>/', views.tNupload, name='tNupload'),  # For edits (same name)
-    path('user/', views.tNuser, name='tNuser'),
+    path('tNcourse/', views.tNcourse, name='tNcourse'),
+    path('tNlogin', views.tNlogin, name='tNlogin'),
     path('serve_docx/<int:NoteID>/', views.serve_docx, name='serve_docx'),
+    path('tNnote', views.tNnote, name='tNnote'),
+    path('tNnote/<int:NoteID>/', views.tNnote, name='tNnote'),
+    path('tNnotes/<str:CourseID>/', views.tNnotes, name='tNnotes'),
+    path('tNregister', views.tNregister, name='tNregister'),
+    path('tNsearch/', views.tNsearch, name='tNsearch'),
+    path('tNupload/', views.tNupload, name='tNupload'),
+    path('tNupload/<int:NoteID>/', views.tNupload, name='tNupload'),  
+    path('tNuser/', views.tNuser, name='tNuser'),
+    path('logout/', views.user_logout, name='logout')    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- rango/views.py
+++ rango/views.py
@@ -6,39 +6,11 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from datetime import datetime
-#from docx2pdf import convert
 from django.contrib import messages 
 from tango_with_django_project import settings
 
 
-@login_required
-def restricted(request):
-    return render(request, 'rango/restricted.html')
-
-# Use the login_required() decorator to ensure only those logged in can
-# access the view.
-@login_required
-def user_logout(request):
-    # Since we know the user is logged in, we can now just log them out.
-    logout(request)
-    # Take the user back to the homepage.
-    return redirect(reverse('rango:index'))
-
-def index(request):
-
-    context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-    
-
-    visitor_cookie_handler(request)
-
-    response = render(request, 'rango/tNindex.html', context=context_dict)
-
-    return response
-
 def tNindex(request):
-
     courses = Courses.objects.all()
     top_notes = Note.objects.order_by('-views')[:5]
     
@@ -59,7 +31,6 @@ def tNlogin(request):
         password = request.POST.get('password')
 
         user = authenticate(username=username, password=password)
-
        
         if user:
             # Is the account active? It could have been disabled.
@@ -76,7 +47,6 @@ def tNlogin(request):
             print(f"Invalid login details: {username}, {password}")
             return HttpResponse("Invalid login details supplied.")
     else:
-    
         return render(request, 'rango/tNlogin.html')
    
 def serve_docx(request, NoteID):
@@ -95,19 +65,21 @@ def serve_docx(request, NoteID):
     else:
         return HttpResponse("File not found", status=404)
 
-def tNnote(request,NoteID):
-    note = Note.objects.get(NoteID = NoteID)
+def tNnote(request, NoteID):
+    note = get_object_or_404(Note, NoteID=NoteID)
 
-    # Check if user hasn't viewed this note before
-    if not note.viewed_by.filter(id=request.user.id).exists():
-        note.views += 1
-        note.viewed_by.add(request.user)
-        note.save()
+    if request.user.is_authenticated:
+        try:
+            if not note.viewed_by.filter(pk=request.user.pk).exists():
+                note.views += 1
+                note.viewed_by.add(request.user)
+                note.save()
+        except (AttributeError, TypeError):
+            pass
 
     return render(request, 'rango/tNnote.html', {'note': note})
 
 def tNnotes(request,CourseID):
-
     notes_from_course = Note.objects.filter(CourseID = CourseID)
 
     return render(request, 'rango/tNnotes.html', {'course_notes':notes_from_course})
@@ -134,7 +106,6 @@ def tNregister(request):
 
             registered = True
             return redirect('rango:tNlogin')  # Redirect to the login page after successful registration
-       
         else:
             print(user_form.errors, students_form.errors)  # Print form errors for debugging
     else:
@@ -175,7 +146,6 @@ def tNupload(request, NoteID = None):
             edit = False
             creator = None
             form = NoteForm(request.POST, request.FILES, user=request.user.students, edited=NoteID)  # request.user.students is the Students instance
-
         
         if form.is_valid():
             
@@ -199,16 +169,12 @@ def tNupload(request, NoteID = None):
             edit = False
             creator = None
             form = NoteForm(user=request.user.students)
-
     
     return render(request, 'rango/tNupload.html', {'form': form, 'edit':edit, 'creator' : creator})
 
 def tNuser(request):
-    
-    notes = Note.objects.filter(user = request.user.id)
-    
-
-    return render(request, 'rango/tNuser.html', {'notes':notes})
+    notes = Note.objects.filter(user=request.user.id)
+    return render(request, 'rango/tNuser.html', {'notes': notes, 'username': request.user.username})
 
 @login_required
 def user_logout(request):
@@ -216,125 +182,4 @@ def user_logout(request):
     return redirect(reverse('rango:tNindex'))
 
 
-def about(request):
-    context_dict = {}
-    visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
-    return render(request, 'rango/about.html', context=context_dict)
 
-
-
-def register(request):
-    # A boolean value for telling the template
-    # whether the registration was successful.
-    # Set to False initially. Code changes value to
-    # True when registration succeeds.
-    registered = False
-
-    # If it's a HTTP POST, we're interested in processing form data.
-    if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
-        user_form = UserForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
-
-        # If the two forms are valid...
-        if user_form.is_valid() and profile_form.is_valid():
-            # Save the user's form data to the database.
-            user = user_form.save()
-
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
-            user.set_password(user.password)
-            user.save()
-
-            # Now sort out the UserProfile instance.
-            # Since we need to set the user attribute ourselves,
-            # we set commit=False. This delays saving the model
-            # until we're ready to avoid integrity problems.
-            profile = profile_form.save(commit=False)
-            profile.user = user
-
-            # Did the user provide a profile picture?
-            # If so, we need to get it from the input form and
-            # put it in the UserProfile model.
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-
-            # Now we save the UserProfile model instance.
-            profile.save()
-
-            # Update our variable to indicate that the template
-            # registration was successful.
-            registered = True
-        else:
-            # Invalid form or forms - mistakes or something else?
-            # Print problems to the terminal.
-            print(user_form.errors, profile_form.errors)
-    else:
-        # Not a HTTP POST, so we render our form using two ModelForm instances.
-        # These forms will be blank, ready for user input.
-        user_form = UserForm()
-        profile_form = UserProfileForm()
-
-    # Render the template depending on the context.
-    return render(request,
-                  'rango/register.html',
-                  context={'user_form': user_form,
-                           'profile_form': profile_form,
-                           'registered': registered})
-
-def user_login(request):
-    # If the request is a HTTP POST, try to pull out the relevant information.
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        
-        user = authenticate(username=username, password=password)
-
-       
-        if user:
-            # Is the account active? It could have been disabled.
-            if user.is_active:
-                # If the account is valid and active, we can log the user in.
-                # We'll send the user back to the homepage.
-                login(request, user)
-                return redirect(reverse('rango:index'))
-            else:
-                # An inactive account was used - no logging in!
-                return HttpResponse("Your Rango account is disabled.")
-        else:
-            # Bad login details were provided. So we can't log the user in.
-            print(f"Invalid login details: {username}, {password}")
-            return HttpResponse("Invalid login details supplied.")
-    else:
-    
-        return render(request, 'rango/login.html')
-
-# A helper method
-def get_server_side_cookie(request, cookie, default_val=None):
-    val = request.session.get(cookie)
-    if not val:
-        val = default_val
-    return val
-
-def visitor_cookie_handler(request):
-    # Get the number of visits from the server-side cookie
-    visits = int(get_server_side_cookie(request, 'visits', '1'))
-
-    # Get the last visit time from the server-side cookie or set it to the current time if it doesn't exist
-    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
-
-    # If it's been more than a day since the last visit...
-    if (datetime.now() - last_visit_time).days > 0:
-        visits = visits + 1
-        # Update the last visit cookie with the current time
-        request.session['last_visit'] = str(datetime.now())
-    else:
-        # Keep the last visit cookie unchanged
-        request.session['last_visit'] = last_visit_cookie
-
-    # Update/set the visits cookie
-    request.session['visits'] = visits
