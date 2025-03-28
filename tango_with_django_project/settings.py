57cbce2228d6db93e585d9ac4ae6eaec197708c8
“Zicheng <2915413y@student.gla.ac.uk>
2025-03-28 03:50:08 +0000

    fix_static_staticfiles_error

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -120,7 +120,7 @@ MEDIA_URL = '/media/'
 
 if ON_PYTHONANYWHERE:
     # PythonAnywhere specific settings
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 else:
     # Local development settings
